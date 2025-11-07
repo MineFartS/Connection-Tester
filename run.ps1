8089bad4a621253921d07c60b11f15a1445185e6
Phil Hunt <philiphuntiv2@gmail.com>
2025-11-07 11:51:49 -0500

    Create run.ps1

---
+++ run.ps1
@@ -0,0 +1,50 @@
+
+$hostname = Read-Host "Hostname"
+
+$host.ui.RawUI.WindowTitle = "Connection Tester"
+
+while ($true) {
+
+    Clear-Host
+
+    Write-Host ''
+
+    Write-Host `
+        "$hostname - " `
+        -NoNewline 
+    
+    Write-Host `
+        "Testing ..." `
+        -ForegroundColor Cyan `
+        -NoNewline
+
+    $online = Test-Connection `
+        -ComputerName $hostname `
+        -Count 1 `
+        -Quiet
+
+    Write-Host `
+        "`r                                                                      " `
+        -NoNewline 
+
+    Write-Host `
+        "`r$hostname - " `
+        -NoNewline 
+
+    if ($online) {
+
+        Write-Host `
+            "Online" `
+            -ForegroundColor Green
+
+    } else {
+
+        Write-Host `
+            "Offline" `
+            -ForegroundColor Red
+
+    }
+
+    timeout.exe 5
+
+}
